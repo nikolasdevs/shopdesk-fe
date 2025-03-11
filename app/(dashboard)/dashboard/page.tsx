fc78bc1928611ae20705238085747c4671aa8323
Emmanuel Esan <nikolasdevs@gmail.com>
2025-03-11 08:45:19 +0100

    updated main

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,29 +1,31 @@
 "use client";
-import ShopDeskModal from "@/components/modal/add-item";
-import { useEffect, useState, useRef } from "react";
-import { ChevronDown, MoreVertical } from "lucide-react";
-import { useRouter } from "next/navigation";
-import EditItemModal from "@/components/modal/edit-stock";
-import AddItemModal from "@/components/modal/add-item";
+import LoadingAnimation from "@/components/functional/loading";
+import Logo from "@/components/functional/logo";
+import {
+  default as AddItemModal,
+  default as ShopDeskModal,
+} from "@/components/modal/add-item";
 import DeleteItem from "@/components/modal/delete-item";
+import EditItemModal from "@/components/modal/edit-stock";
+import LogoutConfirmModal from "@/components/modal/logoutConfirmationModal";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import LogoutConfirmModal from "@/components/modal/logoutConfirmationModal";
-import Image from "next/image";
-import Logo from "@/components/functional/logo";
-import LoadingAnimation from "@/components/functional/loading";
 import {
   Table,
-  TableHeader,
   TableBody,
-  TableRow,
-  TableHead,
   TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table";
+import { ChevronDown, MoreVertical } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import useTableAreaHeight from "./hooks/useTableAreaHeight";
 
 const Page = () => {
@@ -131,14 +133,6 @@ const Page = () => {
     );
   }
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log({ email, password });
-  };
-
   return (
     <main className="px-6 py-4 w-full max-w-7xl mx-auto flex flex-col main-h-svh ">
       <div ref={tableAreaRef} className="space-y-8 w-full h-full ">
@@ -206,41 +200,6 @@ const Page = () => {
           </div>
           <div className="border shadow-md rounded-b-lg rounded-bl-lg relative rounded-tr-lg flex-1">
             {stockItems.length === 0 ? (
-<<<<<<< HEAD
-              <div className="w-full overflow-x-auto">
-                <ul className="flex lg:grid lg:grid-cols-6 overflow-x-auto place-items-center place-content-center p-4 w-full">
-                  <li className="font-semibold text-black text-sm flex items-center gap-3 hover:cursor-pointer">
-                    ITEM NAME
-                  </li>
-                  <li className="font-semibold text-black text-sm flex items-center gap-3 hover:cursor-pointer">
-                    SKU PRICE
-                  </li>
-                  <li className="font-semibold text-black text-sm flex items-center gap-3 hover:cursor-pointer">
-                    PRICE
-                  </li>
-                  <li className="font-semibold text-black text-sm flex items-center justify-center hover:cursor-pointer">
-                    QUANTITY
-                  </li>
-                  <li className="font-semibold text-black text-sm flex items-center justify-center hover:cursor-pointer">
-                    ACTION
-                  </li>
-                  <li className="font-semibold text-black text-xl flex items-center justify-center hover:cursor-pointer">
-                    +
-                  </li>
-                </ul>
-                <span className="w-full h-px bg-[#DEDEDE] block"></span>
-             
-                <div className="relative h-[80vh] w-full">
-                
-              <div className="absolute space-y-4 right-0 left-0 top-28 w-56 mx-auto text-center">
-                    <Image
-                      src="/icons/empty-note-pad.svg"
-                      alt=""
-                      width={56}
-                      height={56}
-                      className="mx-auto"
-                    />
-=======
               <div className="relative">
                 <div className="w-full overflow-x-auto">
                   <ul className="flex items-center justify-between w-full rounded-tr-lg">
@@ -300,108 +259,6 @@ const Page = () => {
                   <p className="text-gray-400 text-sm flex items-center gap-1 justify-center text-center">
                     You have <span className="text-black">0</span> stock
                     (Displaying <span className="text-black">6</span>{" "}
-<<<<<<< HEAD
-                      <Image
-                        src="/icons/ArrowDropDown.svg"
-                        alt=""
-                        width={56}
-                        height={56}
-                        className="mx-auto"
-                      />
-                      per page)
-                   </p>
->>>>>>> upstream/main
-                    <p className="text-[#888888] text-sm">
-                      You have 0 items in stock
-                    </p>
-                    <button onClick={openModal} className="btn-outline hover:cursor-pointer">
-                      + Add New Stock
-                    </button>
-                    <ShopDeskModal isOpen={isOpen} onClose={closeModal} />
-                  </div>
-                      <Dialog>
-                  <DialogContent>
-                    <DialogOverlay className="fixed inset-0 data-[state=open]:animate-overlayShow bg-black/30 -z-[50]" />
-                    <DialogContent className="fixed left-1/2 top-1/2 max-h-[85vh] w-[720px]  -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[var(--shadow-6)] focus:outline-none data-[state=open]:animate-contentShow ">
-                      <DialogTitle>
-                        <div className=" bg-green-400 flex justify-between p-6 ">
-                          <div className="flex items-center gap-4 ">
-                            <Image src={stock} alt="" width={24} height={24} />
-                            <div>
-                              <p>Add New Stock</p>
-                              <p>Always know the items you have available.</p>
-                            </div>
-                          </div>
-                          <Image src={close} alt="" width={16} height={16} />
-                        </div>
-                      </DialogTitle>
-                      <form
-                        onSubmit={handleSubmit}
-                        className="flex flex-col gap-6"
-                      >
-                        <div className="flex flex-col gap-1.5">
-                          <label
-                            htmlFor="email"
-                            className="block text-sm font-medium"
-                          >
-                            Email address{" "}
-                            <span className="text-[#FF1925]">*</span>
-                          </label>
-                          <input
-                            type="email"
-                            id="email"
-                            className="w-full p-3 border rounded-[9px] focus:ring-2 focus:ring-[#CCEBDB] focus:border-[#009A49] outline-none text-[#2A2A2A] "
-                            placeholder="johnwick@gmail.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                          />
-                        </div>
-
-                        <div className="flex flex-col gap-1.5">
-                          <label
-                            htmlFor="password"
-                            className="block text-sm font-medium"
-                          >
-                            Your password{" "}
-                            <span className="text-[#FF1925]">*</span>
-                          </label>
-                          <input
-                            type="password"
-                            id="password"
-                            className="w-full p-3 border rounded-[9px] focus:ring-2 focus:ring-[#CCEBDB] focus:border-[#009A49] outline-none text-[#2A2A2A]"
-                            placeholder="*************"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                          />
-                        </div>
-
-                        <button
-                          type="submit"
-                          className="w-full bg-[#2A2A2A] text-white p-3 rounded-lg font-medium hover:bg-black transition duration-200"
-                        >
-                          Sign in
-                        </button>
-                      </form>
-                      <div className="mt-[25px] flex justify-end">
-                        <DialogClose asChild>
-                          <button className="inline-flex h-[35px] items-center justify-center rounded bg-green4 px-[15px] font-medium leading-none text-green11 outline-none outline-offset-1 hover:bg-green5 focus-visible:outline-2 focus-visible:outline-green6 select-none">
-                            Save changes
-                          </button>
-                        </DialogClose>
-                      </div>
-                      <DialogClose asChild>
-                        <button
-                          className="absolute right-2.5 top-2.5 inline-flex size-[25px] appearance-none items-center justify-center rounded-full text-violet11 bg-gray3 hover:bg-violet4 focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none"
-                          aria-label="Close"
-                        ></button>
-                      </DialogClose>
-                    </DialogContent>
-                  </DialogContent>
-                  
-                </Dialog>
-=======
                     <Image
                       src="/icons/ArrowDropDown.svg"
                       alt=""
@@ -412,9 +269,7 @@ const Page = () => {
                     per page)
                   </p>
                 </div>
->>>>>>> upstream/main
               </div>
-              
             ) : (
               <Table className="border-collapse  overflow-y-auto">
                 <TableHeader>
