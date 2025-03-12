555d31d3668b0331e8fc3eebe1bf431e0aad94d9
Emmanuel Esan <nikolasdevs@gmail.com>
2025-03-12 12:44:08 +0100

    icon added

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -21,7 +21,7 @@ import {
 } from "@/components/ui/table";
 import box from "@/public/icons/box.svg";
 import { deleteStock, GetStock } from "@/services/stock";
-import { ChevronDown, Search } from "lucide-react";
+import { ChevronDown, MoreVertical, Search } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
