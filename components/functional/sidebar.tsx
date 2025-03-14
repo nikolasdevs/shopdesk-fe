be58b7e73631d5cfcb440fac06671e8b2025c6f7
Siddami <dharmmy621@gmail.com>
2025-03-14 18:19:34 +0100

    fix: make sidebar responsive on all breakpoints

--- components/functional/sidebar.tsx
+++ components/functional/sidebar.tsx
@@ -72,7 +72,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
 
   return (
     <>
-      <div className="fixed inset-0 md:relative  w-full max-w-[344px] md:max-w-[356px] bg-white transition-transform duration-300 ease-in-out transform translate-x-0 flex flex-col flex-grow  items-start rounded-xl md:border md:border-[#DEE5ED] ml-4 mr-[15px] md:m-0">
+      <div className="fixed inset-x-[-15] inset-y-[58] md:inset-0 md:relative w-full md:max-w-[356px] bg-white transition-transform duration-300 ease-in-out transform translate-x-0 flex flex-col flex-grow  items-center justify-center rounded-xl md:border md:border-[#DEE5ED] ml-4 mr-[15px] md:m-0 overflow-y-auto ">
         <div className="hidden md:flex py-5.5 px-4 items-center justify-between border-b border-b-[#DEE5ED] w-full">
           <p className="font-circular-medium text-2xl leading-9">
             {selectedItem.name}
@@ -83,20 +83,20 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
         </div>
 
         {/* Mobile */}
-        <div className="flex items-center justify-between border-b border-b-[#DEE5ED] w-full md:hidden">
+        <div className="flex items-center justify-between border-b border-b-[#DEE5ED] w-full md:hidden p-3">
           <p className="font-circular-medium text-xl leading-9">
-            Edit Stock
+            Edit stock
           </p>
-          <button onClick={onClose} className="p-[7px] bg-neutral-200 rounded-md">
-            <X size={13} />
+          <button onClick={onClose} className="p-[7px] bg-neutral-100 rounded-lg">
+            <X size={13} className="text-neutral-600" />
           </button>
         </div>
 
-        <div className="flex flex-col md:py-5 md:px-4 items-start gap-5 w-full">
+        <div className="flex flex-col md:py-5 md:px-4 items-start gap-5 w-full p-3">
           {/* Product */}
-          <div className="flex p-3 items-start gap-5 rounded-md w-full ">
+          <div className="flex p-3 items-center justify-between gap-5 rounded-md w-full border-b border-b-[#E9EEF3] md:border-none md:bg-[#F8FAFB]">
             <div className="flex flex-col gap-1 w-2/3">
-              <p className="text-[#717171] text-base font-circular-normal leading-7">
+              <p className="text-[#717171] text-sm md:text-base font-circular-normal leading-7">
                 Product name
               </p>
               <p className="text-[#2A2A2A] text-lg leading-7.5 font-circular-normal">
@@ -112,7 +112,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
           </div>
 
           {/* Price */}
-          <div className="flex p-3 items-start gap-5 rounded-md md:border md:border-[#E9EEF3] md:bg-[#F8FAFB] w-full">
+          <div className="flex p-3 items-center justify-between gap-5 rounded-md border-b border-b-[#E9EEF3] md:border-none md:bg-[#F8FAFB] w-full">
             <div className="flex flex-col gap-1 w-2/3">
               <p className="text-[#717171] text-base md:text-lg font-circular-normal leading-7" 
               >
@@ -141,7 +141,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
           </div>
 
           {/* Quantity */}
-          <div className="flex p-3 items-start gap-5 rounded-md md:border md:border-[#E9EEF3] md:bg-[#F8FAFB] w-full">
+          <div className="flex p-3 items-center justify-between gap-5 rounded-md border-b border-b-[#E9EEF3] md:border-none md:bg-[#F8FAFB] w-full">
             <div className="flex flex-col gap-1 w-2/3">
               <p className="text-[#717171] text-base md:text-lg font-circular-normal leading-7">
                 Quantity
@@ -159,7 +159,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
           </div>
 
           {/* Image */}
-          <div className="flex p-3 items-start gap-5 rounded-md md:border md:border-[#E9EEF3] md:bg-[#F8FAFB] w-full">
+          <div className="flex p-3 items-center justify-between gap-5 rounded-md border-b border-b-[#E9EEF3] md:border-none md:bg-[#F8FAFB] w-full">
             <div className="flex flex-col gap-1 w-2/3">
               <p className="text-[#717171] text-base md:text-lg font-circular-normal leading-7">
                 Image
@@ -189,8 +189,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
               className="text-black md:text-[#009A49] font-circular-normal text-sm md:text-base leading-6 cursor-pointer md:w-1/3 text-right border border-[#A0A0A0] rounded-xl py-3 px-6 md:py-0 md:px-0 md:border-none"
               onClick={()=>{
                 selectedItem.images && selectedItem.images.length > 0
-                  ? openEditImageModal
-                  : openImageUploader                
+                  ? openEditImageModal()
+                  : openImageUploader()                
               }
               }
             >
@@ -204,7 +204,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
         <div className="w-full h-full p-4 flex flex-col md:hidden gap-4 mt-11">
           {/* Save Button */}
           <Button
-            className="bg-[#D0D0D0] text-white text-base border border-[#B8B8B8] py-2 pr-6 pl-4"
+            className="bg-[#B8B8B8] text-white text-base font-circular-medium py-6 px-2 rounded-lg hover:border-b hover:border-b-primary transition-colors"
             variant="default"
             fullWidth
           >
@@ -213,7 +213,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
 
           {/* Delete Button */}
           <Button
-            className="bg-white text-[#FF000D] text-base border border-[#E50000] py-2 pr-6 pl-4"
+            className="bg-white text-[#FF000D] text-base font-circular-medium px-2 py-6 border border-[#FF000D] rounded-lg hover:bg-[#FF000D] hover:text-white transition-colors"
             variant="default"
             fullWidth
           >
@@ -234,7 +234,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
       )}
 
 
-   {/**{isEditModalOpen && (
+     {isEditModalOpen && (
         <EditStockV3Modal
           isOpen={isEditModalOpen}
           item={selectedItem} 
@@ -246,7 +246,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, selectedItem, onSave
           openSuccessModal={openSucessModal}
           />
       )}
-      */}
+
       {isEditQuantityOpen && (
         <EditQuantityModal
           isOpen={isEditQuantityOpen}
