ca55b01e077aa82e1dc123ab79d52f3b890a3757
Siddami <dharmmy621@gmail.com>
2025-03-23 11:26:45 +0100

    feat: uncommented datatable component, and commented out code for api data and used static data

--- app/(app)/stock/page.tsx
+++ app/(app)/stock/page.tsx
@@ -6,29 +6,30 @@ import { DataTable } from './components/data-table';
 import { useGetUserQuery } from '@/redux/features/auth/auth.api';
 import { useAppSelector } from '@/redux/hooks';
 import { useEffect } from 'react';
+import data from './data/stocks.json'
 // import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 // import { useCreateStockMutation } from '@/redux/features/stock/stock.api';
 // import { startStocksLoading } from '@/redux/features/stock/stock.slice';
 
 export default function StockPage() {
-  const { orgId } = useAppSelector((state) => state.auth);
+  // const { orgId } = useAppSelector((state) => state.auth);
 
-  const [getStock, { data }] = useGetStocksMutation();
+  // const [getStock, { data }] = useGetStocksMutation();
 
-  useEffect(() => {
-    getStock(orgId ?? '160db8736a9d47989381e01a987e4413')
-      .unwrap()
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   getStock(orgId ?? '160db8736a9d47989381e01a987e4413')
+  //     .unwrap()
+  //     .then((response) => {
+  //       console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
 
   return (
     <div className='container'>
-      {/* <DataTable data={data} columns={columns} /> */}
+      <DataTable data={data} columns={columns} />
     </div>
   );
 }
